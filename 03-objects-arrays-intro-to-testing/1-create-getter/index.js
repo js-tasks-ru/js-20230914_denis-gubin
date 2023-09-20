a4fa66be7ab1a7ce9a699847b9aac89cffebe306
denisgubin <denis.gubin@gmail.com>
2023-09-20 18:18:28 +0300

    task3-01 task3-02 task3-03 task3-04

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,19 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  let partParts = path.split(".");
 
+  return function deepIntoObj(obj) {
+    if (typeof obj !== 'object') {
+      return obj;
+    }
+
+    let keys = Object.keys(obj);
+    for (let i = 0; i < keys.length;i++) {
+      if (keys[i] === partParts[i]) {
+        partParts = partParts.slice(1);
+        return deepIntoObj(obj[keys[i]]);
+      }
+    }
+  };
 }
--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -4,5 +4,19 @@
  * @returns {*[]} - the new array with uniq values
  */
 export function uniq(arr) {
+  if (typeof arr === 'undefined' || arr.length === 0) { // вот здесь и выстреливает динамическая типизация, что только в runtime можно понять, передали ли arr или нет
+    return [];
+  }
 
+  let uniqArr = [];
+  let tmpMap = new Map();
+
+  for (const elem of arr) {
+    if (!tmpMap.has(elem)) {
+      uniqArr.push(elem);
+      tmpMap.set(elem, null);
+    }
+  }
+
+  return uniqArr;
 }
