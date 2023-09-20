7294b70521ff863084edd97854e7a55f7435db7a
denisgubin <denis.gubin@gmail.com>
2023-09-20 23:04:06 +0300

    task02 задача 01 Вернулся к старому решению

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -11,19 +11,27 @@ const localeRu = "ru-RU";
 export function sortStrings(arr, param = 'asc') {
   let copyArray = [...arr];
 
-  return copyArray.sort(compare(param));
+  // return copyArray.sort(compare(param));
+  switch (param) {
+  case "desc":
+    return copyArray.sort((a, b) => b.localeCompare(a, [localeRu + "-u-kf-upper", localeEn + "-u-kf-upper"], {sensitivity: 'case'}));
+  case "asc":
+    return copyArray.sort((a, b) => a.localeCompare(b, [localeRu + "-u-kf-upper", localeEn + "-u-kf-upper"], {sensitivity: 'case'}));
+  default:
+    return arr; // unknown criteria neither "asc" or "desc" then return with not sorted array
+  }
 }
 
-// можно ли в js обходится без стрелочных функций, это же не будет not js style ?
-function compare(setDirection) {
-  if (setDirection === 'desc') {
-    return function (a, b) {
-      return b.localeCompare(a, [localeRu + "-u-kf-upper", localeEn + "-u-kf-upper"], {sensitivity: 'case'});
-    };
-  }
-  return function (a, b) {
-    return a.localeCompare(b, [localeRu + "-u-kf-upper", localeEn + "-u-kf-upper"], {sensitivity: 'case'});
-  };
-}
+// // можно ли в js обходится без стрелочных функций, это же не будет not js style ?
+// function compare(setDirection) {
+//   if (setDirection === 'desc') {
+//     return function (a, b) {
+//       return b.localeCompare(a, [localeRu + "-u-kf-upper", localeEn + "-u-kf-upper"], {sensitivity: 'case'});
+//     };
+//   }
+//   return function (a, b) {
+//     return a.localeCompare(b, [localeRu + "-u-kf-upper", localeEn + "-u-kf-upper"], {sensitivity: 'case'});
+//   };
+// }
 
 
